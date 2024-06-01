bb5067cd42192c622de62cb18636e58e1b1dadb6
Fariz Irvansyah <145618268+farizirvansyah@users.noreply.github.com>
2024-06-02 00:36:57 +0700

    Add files via upload

---
+++ Fragment/java/com/example/a2211500141_fragment/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.a2211500141_fragment;
+
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        //< create >
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //</ create >
+
+        // < get elements ›
+        TabLayout tabLayout = findViewById(R.id.tabs);
+        ViewPager2 viewPager2 = findViewById(R.id.view_pager);
+        // </ get elements ›
+
+        ViewPagerAdapter adapter = new ViewPagerAdapter(fragmentActivity: this);
+        viewPager2.setAdapter(adapter);
+
+        new TabLayoutMediator(tabLayout, viewPager2,
+            new TabLayoutMediator.TabConfigurationStrategy() {
+                @Override
+                public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+                    tab.setText("Tab " + (position + 1));
+                }
+            }}.attach();
+}
+}
